0f68b706c2619a4ddcd04abeb3d62ff5e144a0d6
kortox <kortox@gmail.com>
2021-01-16 12:55:07 -0800

    clock basically works but is written out horizontally

--- src/main.rs
+++ src/main.rs
@@ -14,101 +14,104 @@ use std::{thread, time};
 pub struct App {
     gl: GlGraphics, // OpenGL drawing backend.
     // rotation: f64,  // Rotation for the square.
-    which_glyph: i32,
+    right_now: DateTime<Local>,
 }
 
 type GlyphComponent = [f64; 4];
-
 type Glyph = Vec<GlyphComponent>;
 
+// Expects numbers used in dates & times i.e. in [0, 59]
+fn split_date_comp_to_digits(n: u32) -> [u32; 2] {
+    assert!(n < 60);
+    let ones = n % 10;
+    let tens = n / 10;
+    return [tens, ones];
+}
 
 impl App {
     fn render(&mut self, args: &RenderArgs) {
         use graphics::*;
 
-        // const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
-        // const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
-        // const BLUE: [f32; 4] = [0.0, 0.0, 1.0, 1.0];
         const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
         const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
 
         // vertical line
-        const ZERO_VERT:  GlyphComponent = [0.0, 0.0, 0.0, 10.0];
+        const ZERO_VERT: GlyphComponent = [0.0, 0.0, 0.0, 10.0];
         // top horizontal line
-        const ONE_HORIZ:  GlyphComponent = [0.0, 0.0, 10.0, 0.0];
+        const ONE_HORIZ: GlyphComponent = [0.0, 0.0, 10.0, 0.0];
         // lower horizontal line
-        const TWO_HORIZ:  GlyphComponent = [0.0, 10.0, 10.0, 10.0];
+        const TWO_HORIZ: GlyphComponent = [0.0, 10.0, 10.0, 10.0];
         // backslash line from top
-        const THREE_SLASH:  GlyphComponent = [0.0, 0.0, 10.0, 10.0];
+        const THREE_SLASH: GlyphComponent = [0.0, 0.0, 10.0, 10.0];
         // forward slash line from top
-        const FOUR_SLASH:  GlyphComponent = [0.0, 10.0, 10.0, 0.0];
+        const FOUR_SLASH: GlyphComponent = [0.0, 10.0, 10.0, 0.0];
         // vert line out
-        const SIX_VERT:  GlyphComponent = [10.0, 0.0, 10.0, 10.0];
-        let ALL_GLYPH_COMPS: [GlyphComponent; 6] = [ZERO_VERT, ONE_HORIZ, TWO_HORIZ, THREE_SLASH, FOUR_SLASH, SIX_VERT];
+        const SIX_VERT: GlyphComponent = [10.0, 0.0, 10.0, 10.0];
 
-        let ZERO: Glyph = vec![ZERO_VERT];
-        let ONE: Glyph = vec![ZERO_VERT, ONE_HORIZ];
-        let TWO: Glyph = vec![ZERO_VERT, TWO_HORIZ];
-        let THREE: Glyph = vec![ZERO_VERT, THREE_SLASH];
-        let FOUR: Glyph = vec![ZERO_VERT, FOUR_SLASH];
-        let FIVE: Glyph = vec![ZERO_VERT, FOUR_SLASH, ONE_HORIZ];
-        let SIX: Glyph = vec![ZERO_VERT, SIX_VERT];
-        let SEVEN: Glyph = vec![ZERO_VERT, SIX_VERT, ONE_HORIZ];
-        let EIGHT: Glyph = vec![ZERO_VERT, SIX_VERT, TWO_HORIZ];
-        let NINE: Glyph = vec![ZERO_VERT, SIX_VERT, ONE_HORIZ, TWO_HORIZ];
-        let ALL_GLYPHS = vec![ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE];
-
+        let zero: Glyph = vec![ZERO_VERT];
+        let one: Glyph = vec![ZERO_VERT, ONE_HORIZ];
+        let two: Glyph = vec![ZERO_VERT, TWO_HORIZ];
+        let three: Glyph = vec![ZERO_VERT, THREE_SLASH];
+        let four: Glyph = vec![ZERO_VERT, FOUR_SLASH];
+        let five: Glyph = vec![ZERO_VERT, FOUR_SLASH, ONE_HORIZ];
+        let six: Glyph = vec![ZERO_VERT, SIX_VERT];
+        let seven: Glyph = vec![ZERO_VERT, SIX_VERT, ONE_HORIZ];
+        let eight: Glyph = vec![ZERO_VERT, SIX_VERT, TWO_HORIZ];
+        let nine: Glyph = vec![ZERO_VERT, SIX_VERT, ONE_HORIZ, TWO_HORIZ];
+        let ALL_GLYPHS: Vec<Glyph> =
+            vec![zero, one, two, three, four, five, six, seven, eight, nine];
 
         let bg_color: [f32; 4] = BLACK;
         let fg_color: [f32; 4] = WHITE;
 
-        //let rotation = self.rotation;
-        let which_glyph = self.which_glyph;
         // center of window
-        let (center_x, center_y) = (args.window_size[0] / 2.0, args.window_size[1] / 2.0);
+        // let (center_x, center_y) = (args.window_size[0] / 2.0, args.window_size[1] / 2.0);
+        let (init_x, init_y) = (10.0, args.window_size[1] / 2.0);
+        let right_now = self.right_now;
 
         self.gl.draw(args.viewport(), |c, gl| {
             // Clear the screen.
             clear(bg_color, gl);
 
-            // let transform = c
-            //     .transform
-            //     .trans(center_x, center_y)
-            //     .rot_rad(rotation)
-            //     .trans(-25.0, -25.0);
+            let date_comps = vec![
+                split_date_comp_to_digits(right_now.time().hour() as u32),
+                split_date_comp_to_digits(right_now.time().minute() as u32),
+                split_date_comp_to_digits(right_now.time().second() as u32),
+                split_date_comp_to_digits(right_now.date().day() as u32),
+                split_date_comp_to_digits(right_now.date().month() as u32),
+                split_date_comp_to_digits((right_now.date().year() / 100) as u32),
+                split_date_comp_to_digits((right_now.date().year() % 100) as u32),
+            ];
 
-            let center_trans = c.transform.trans(center_x, center_y);
-
-            // Draw a box rotating around the middle of the screen.
-            for glyph_component in ALL_GLYPHS[which_glyph as usize].to_owned() {
-                line(fg_color, 1.0, glyph_component, center_trans, gl);
+            let mut curr_x = init_x;
+            let curr_y = init_y;
+            for date_comps_as_digits in &date_comps {
+                for digit in date_comps_as_digits {
+                    let curr_trans = c.transform.trans(curr_x, curr_y);
+                    let idx: usize = *digit as usize;
+                    for glyph_component in ALL_GLYPHS[idx as usize].to_owned() {
+                        line(fg_color, 1.0, glyph_component, curr_trans, gl);
+                    }
+                    curr_x += 15.0;
+                }
+                curr_x += 5.0;
             }
-
         });
     }
 
     fn update(&mut self, args: &UpdateArgs) {
         // Rotate 2 radians per second.
         // self.rotation += 2.0 * args.dt;
-        self.which_glyph += 1;
-        self.which_glyph %= 10;
+        self.right_now = Local::now();
     }
 }
 
 fn main() {
-    let local: DateTime<Local> = Local::now();
-
-    println!("hour: {}", local.time().hour());
-    println!("minute: {}", local.time().minute());
-    println!("day: {}", local.date().day());
-    println!("month: {}", local.date().month());
-    println!("year: {}", local.date().year());
-
     // Change this to OpenGL::V2_1 if not working.
     let opengl = OpenGL::V3_2;
 
     // Create an Glutin window.
-    let mut window: Window = WindowSettings::new("cistercian clock", [200, 200])
+    let mut window: Window = WindowSettings::new("cistercian clock", [250, 250])
         .graphics_api(opengl)
         .exit_on_esc(true)
         .build()
@@ -118,14 +121,13 @@ fn main() {
     let mut app = App {
         gl: GlGraphics::new(opengl),
         // rotation: 0.0,
-        which_glyph: 0,
+        right_now: Local::now(),
     };
 
     let mut events = Events::new(EventSettings::new());
     while let Some(e) = events.next(&mut window) {
-
-        let ten_millis = time::Duration::from_millis(250);
-        thread::sleep(ten_millis);
+        // let ten_millis = time::Duration::from_millis(250);
+        // thread::sleep(ten_millis);
 
         if let Some(args) = e.render_args() {
             app.render(&args);
